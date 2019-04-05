317849e553fc3c12d129cc09967196ccb6dc36ec
Christoffer Nilsson <chriszmn@gmail.com>
2019-04-05 08:30:30 +0200

    new branch

--- reddit/spiders/test_spider.py
+++ reddit/spiders/test_spider.py
@@ -7,7 +7,9 @@ from time import sleep
 
 class TestSpider(scrapy.Spider):
     name = "test"
-    start_urls = ['https://www.reddit.com/r/AskReddit/comments/b0e6ty/whats_an_oh_shit_moment_where_you_realised_youve/']
+    start_urls = ['https://www.reddit.com/r/cloudbells/comments/b8yvk7/test2/']
+
+
 
     def __init__(self):
         self.driver = webdriver.Firefox(executable_path='geckodriver.exe')
@@ -16,55 +18,23 @@ class TestSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
         
-        sleep(60)
+        sleep(10)
 
-        page_loaded = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@id, 'moreComments')]"))
-        )
+        if True:
+            cookiesBtn = self.driver.find_element_by_xpath("//button[@type='submit'][contains(text(), 'I Agree')]")
+            cookiesBtn.click()
 
-        if page_loaded:
-            print("----------------------------------------------------------It worked")
-            print("----------------------------------------------------------It worked")
-            print("----------------------------------------------------------It worked")
-            print("----------------------------------------------------------It worked")
-            print("----------------------------------------------------------It worked")
-            
-            filename = 'test.html'
-            print(self.driver.page_source)
-            with open(filename, 'a', encoding='utf8') as f:
-                f.write(self.driver.page_source)
-
-            # elements = self.driver.find_elements((By.XPATH, "//*[contains(@id, 'moreComments')]"))
-            elements = self.driver.find_elements_by_xpath("//*[contains(@id, 'moreComments')]")
-            # print(elements)
-            print("-------------------------------")
-            print(len(elements))
-            print("-------------------------------")
-            print("LOOK HERE")
-            divs = self.driver.find_element_by_xpath("//*[contains(@id, 'SHORTCUT_FOCUSABLE_DIV')]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/div[5]/div/div/div/div[400]")
-            # print(len(divs))
-            print(divs)
-
-            clickdiv = self.driver.find_elements_by_xpath("//*[contains(@id, 'SHORTCUT_FOCUSABLE_DIV')]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/div[5]/div/div/div/div[400]")[0]
-            clickdiv.click()
-            print(clickdiv)
-            sleep(5)
-            filename = 'test2.html'
-            with open(filename, 'a', encoding='utf8') as f:
-                f.write(self.driver.page_source)
-            
-            for e in elements:
+            continueLinks = self.driver.find_elements_by_xpath("//span[text()='Continue this thread']/../@href")
+            for c in continueLinks:
                 try:
-                    e.click()
+                    print("???????????????????????????????????????????????????????")
+                    print(c)
+                    print(str(c))
+                    print("???????????????????????????????????????????????????????")
+                    self.driver.get(str(c))
                 except Exception as ex:
-                    print("caught" + str(ex))
+                    print("caught " + str(ex))
                     continue
-                    
-            sleep(5)
-            filename = 'test3.html'
-            with open(filename, 'a', encoding='utf8') as f:
-                f.write(self.driver.page_source)
-            print("-------------------------------")
 
         else:
             print("----------------------------------------------------------It didn't")
@@ -72,4 +42,6 @@ class TestSpider(scrapy.Spider):
             print("----------------------------------------------------------It didn't")
             print("----------------------------------------------------------It didn't")
             print("----------------------------------------------------------It didn't")
+        
+        
         
